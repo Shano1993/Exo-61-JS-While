7d764871dc245dbc0d2fcc220b7d52179273433d
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-15 16:42:57 +0100

    js

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,8 +1,8 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-var i = 0;
-(i  10) {
-    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
+let i = 0;
+while(i < 15) {
+    document.getElementById('monDiv').innerHTML ="Ma variable i vaux " +i;
     i++;
 }
 
--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,9 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 var i = 15;
-while (i   ) {
-    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+while (i <= 45) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux " +i;
+    i += 15;
 }
 
 
